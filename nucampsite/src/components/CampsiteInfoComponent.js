3ceeae0045e034561d0d40559a5da8d6969eef23
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-11-11 05:52:04 -0500

    feat: saving current state before I touch the DirectoryComponent.js file

---
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -0,0 +1,30 @@
+
+// Create a class component named CampsiteInfo inside it, and export it as the default
+
+import React, { Component } from 'react';
+//import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class CampsiteInfo extends Component { // Create a renderSelectedCampsite method that will render the selected campsite
+    constructor(props, campsite) {
+        this.campsite = {
+            campsite: null // Set the campsite to null
+            // if campsite is not null, set the campsite to the campsite object
+        };
+    };
+
+    render() { // Render the campsite info
+        // can the campsite object be returned as truthy? return null if not
+        if (this.campsite) { // If the campsite object is truthy, render the campsite info
+            return (
+                <div className="row" />
+            );
+        }
+        // If not, return an empty <div> with no classes applied to it.
+        return <div />;
+    }
+}
+
+
+// Export the CampsiteInfo component
+export default CampsiteInfo;
