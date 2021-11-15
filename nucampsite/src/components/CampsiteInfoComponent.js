b6f07bd8210268d797695763c1016ba487902553
Jose Torres <vidojam@gmail.com>
2021-11-15 11:08:43 -0500

    Workshop 2 final

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-
-
  
 class CampsiteInfo extends Component { 
     
     constructor(props) {
         super(props)
-        this.state = {
-            campsite:null,
+        this.state = {   
+            Campsite:null           
         };
     }  
 
@@ -20,18 +18,20 @@ class CampsiteInfo extends Component {
             return (
                 <div className = "col-md-5 m-1">
                     <h4>Comments</h4> 
-                      {comments.map(comment => {
-                        <div key={comment.id}>
-                            <p>{comment.text}<br/>
-                             {comment.author}{comment.rating}{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                       {comments.map(comment => {
+                           return (
+                          <div key={comment.id}> 
+                          <p>{comment.text}<br/>
+                            {comment.author}{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                           </p>
-                        </div>;
-                       })}
+                          </div>
+                           )
+                      })}
                  </div>                              
             )
         }        
         return (
-        <div />  
+        <div />
         )
     }
 
@@ -54,11 +54,15 @@ class CampsiteInfo extends Component {
         if (this.props.campsite) {      
             return (
              <div className="row">     
-                {this.renderCampsite(this.props.campsite)} 
-                {this.renderComments(this.props.campsite.comments)}
+                {this.renderCampsite(this.props.campsite)};
+                {this.renderComments(this.props.campsite.comments)};
             </div>
-            )} 
-        return <div />         
+            )
+            } else{
+        return (   
+        <div />
+        )
+       }         
     }
 }
 
