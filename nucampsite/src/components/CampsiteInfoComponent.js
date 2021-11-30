7ff681d18ee32dd2a6fcc9138bb10e4fb2a5b67c
Jose Torres <vidojam@gmail.com>
2021-11-30 11:45:06 -0500

    Fetch  Post Comment 1

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -30,7 +30,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -109,7 +109,7 @@ function RenderCampsite({campsite}) {
 }
 
   
-function RenderComments({comments, addComment, campsiteId}) {     
+function RenderComments({comments, postComment, campsiteId}) {     
     if (comments) {   
         return (
             <div className="col-md-5 m-1">
@@ -123,7 +123,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} addComment={postComment} />
             </div>                              
         );
     }        
@@ -169,7 +169,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
